3bed3723c3ec5ace2846114518395ed2651c0d05
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-06 17:22:31 +0200

    Topbar

--- app/src/main/java/upm/bottomnavigationtutorial/MainActivity.java
+++ app/src/main/java/upm/bottomnavigationtutorial/MainActivity.java
@@ -6,9 +6,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // On create function calls everything when application opens
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,44 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new ProductsFragment()).commit();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
+    // On create function calls everything when application opens
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.topbar, menu);
+        return true;
+    }
+
+
+    // Get passed the item and check which item was clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filter:
+                Toast.makeText(this, "Filter selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.search:
+                Toast.makeText(this, "Search selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.sort:
+                Toast.makeText(this, "Sort selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+
+    // Listeners for the buttons on the bottom navigation bar. When clicking on one of the Icons, opens a new page
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {

a2a524051ff91c8b0ccd14564c4e4288fda02e19
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-09 18:35:46 +0200

    filter layout

--- app/src/main/java/upm/bottomnavigationtutorial/MainActivity.java
+++ app/src/main/java/upm/bottomnavigationtutorial/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {

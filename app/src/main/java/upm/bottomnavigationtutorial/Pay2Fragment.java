d4ce7509700cc78cbe7ff764e186aada3126cb7a
Stoffander <34314082+Stoffander@users.noreply.github.com>
2019-05-17 18:59:20 +0200

    payment screens

---
+++ app/src/main/java/upm/bottomnavigationtutorial/Pay2Fragment.java
@@ -0,0 +1,39 @@
+package upm.bottomnavigationtutorial;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().setTitle("Payment");
+
+        //Adds the back arrow
+
+        ((MainActivity) getActivity()).removeArrow();
+        ((MainActivity) getActivity()).addArrow();
+
+        return inflater.inflate(R.layout.fragment_pay, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.topbar_pay, menu);
+        return;
+    }
+}
